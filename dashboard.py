58254b6bb84d2250d95bcc9da4351c562ebae564
RP <remy.pottier@imt-atlantique.fr>
2021-10-21 14:18:32 +0200

    update the default port

--- dashboard.py
+++ dashboard.py
@@ -58,4 +58,4 @@ def page_not_found(e):
 if __name__ == "__main__":
     logging.basicConfig(filename="info_dashboard.log", level=logging.INFO,
         format="%(asctime)s %(levelname)-8s %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
-    dashboard.run(port=8000, host="0.0.0.0")
+    dashboard.run(port=8081, host="0.0.0.0")
